ac30c335506042f5761f51eefa3ceec62f039b55
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-05-07 23:51:15 -0400

    lesson_1_9

---
+++ src/TodoContainer.js
@@ -0,0 +1,115 @@
+import React from "react";
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./TodoList";
+
+/*The component that works with API and adds, deletes and fetches the data from there */
+
+function TodoContainer() {
+  const [todoList, setTodoList] = React.useState([]);
+
+  /*hook that help to appear the "loading..." message in the add during waiting fetching data */
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  /*getting the infirmation from remote site*/
+  const fetchData = async () => {
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+    };
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      const todos = data.records.map((todo) => {
+        return { id: todo.id, title: todo.fields.title };
+      });
+      setTodoList(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  /*hook that fetching data from API */
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  /*posting new todo on remote site*/
+  const addTodo = async (title) => {
+    const postTitle = {
+      fields: {
+        title: title,
+      },
+    };
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+      body: JSON.stringify(postTitle),
+    };
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`Error has ocurred: ${response.status}`);
+      }
+      const todo = await response.json();
+      const newTodo = { id: todo.id, title: todo.fields.title };
+      setTodoList([...todoList, newTodo]);
+    } catch (error) {
+      console.log(error.message);
+      return null;
+    }
+  };
+
+  /*function for removing todo item from remote site*/
+
+  const removeTodo = async (id) => {
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/${id}`;
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+    };
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`Error has ocurred: ${response.status}`);
+      }
+      const newTodoList = todoList.filter(function (todo) {
+        return id !== todo.id;
+      });
+      setTodoList(newTodoList);
+    } catch (error) {
+      console.log(error.message);
+      return null;
+    }
+  };
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <AddTodoForm onAddTodo={addTodo} />
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : todoList.length ? (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      ) : (
+        <h2 style={{ textAlign: "center" }}>Good job!</h2>
+      )}
+    </>
+  );
+}
+
+export default TodoContainer;
