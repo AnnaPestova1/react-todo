4cef8fe71c3eae0f01daafaa49e7fcec70d96b32
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-05-29 17:54:52 -0400

    add_sort_by_JavaScript_desc

--- src/components/TodoContainer/TodoContainer.js
+++ src/components/TodoContainer/TodoContainer.js
@@ -31,10 +31,10 @@ function TodoContainer({ tableName, baseName, apiKey }) {
       }
       const data = await response.json();
       function sortData(a, b) {
-        if (a.title > b.title) {
+        if (a.title < b.title) {
           return 1;
         }
-        if (a.title < b.title) {
+        if (a.title > b.title) {
           return -1;
         }
         return 0;
