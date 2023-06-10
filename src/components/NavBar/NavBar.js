571be4576b9bed85cce912add4f144ec13639c86
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-06-10 01:49:39 -0400

    Navbar v.2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,48 @@
+import react, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Menu } from "../../img/menu_FILL0_wght400_GRAD0_opsz48.svg";
 import style from "./NavBar.module.css";
-
+console.log(document);
 function NavBar() {
-  return window.innerWidth < 991 ? (
-    <Menu />
-  ) : (
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    const handleResizeWindow = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
+  return (
     <div className={style.NavBar}>
-      <Link to="/home">Home</Link>
-      <Link to="/new">Todo</Link>
+      {screenSize < 767 ? (
+        <div
+          className={style.MobileMenu}
+          onClick={() => setShowMenu((showMenu) => !showMenu)}
+        >
+          {showMenu ? (
+            <ul className={style.MobileMenuLinks}>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/new">Todo</Link>
+              </li>
+            </ul>
+          ) : (
+            <div>
+              <Menu />
+            </div>
+          )}
+        </div>
+      ) : (
+        <>
+          <Link to="/home">Home</Link>
+          <Link to="/new">Todo</Link>
+        </>
+      )}
     </div>
   );
 }
